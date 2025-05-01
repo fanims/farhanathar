95d27c14acfde216f2d8228d503500284f1952bc
amentotech023 <amentotech023@gmail.com>
2025-05-01 16:46:44 +0500

    start new project

---
+++ src/layouts/header.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+function header() {
+  return (
+    <Fragment>
+        <div className='flex justify-center'>
+            <strong>Farhanathar</strong>
+        </div>
+    </Fragment>
+  )
+}
+
+export default header
